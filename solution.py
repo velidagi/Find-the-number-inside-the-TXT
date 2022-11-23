6ea31a7670b9f9e65cf3e0178ff0f47534a77347
Veli Dağı <76882469+velidagi@users.noreply.github.com>
2022-11-23 21:24:39 +0300

    Forum cevap

---
+++ solution.py
@@ -0,0 +1,24 @@
+#txt dosyası okunuyor.
+file1 = open('myfile.txt', 'r')
+
+Lines = file1.readlines()
+
+
+
+line_counter = 0
+
+# Her satıra göre ayırma işlemi yapılıyor.
+for line in Lines:
+
+    line_counter += 1
+
+    a = line.split(")")
+    # a = Kapalı parantez ")" gördüğü her yerde satırdaki cümleyi bölüp kendisine kayıt ediyor.
+
+    for i in a:
+
+        if input_numara == i
+
+            # Verilen numaralı satıra ulaşıldı. Bu satırı silebilirsin
+
+            print(line_counter)
